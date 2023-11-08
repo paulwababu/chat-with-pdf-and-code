fdff7205f1453c29b197f22920597d498eb09f54
paulwababu <paulkiragu621@gmail.com>
2023-11-08 17:34:14 +0300

    bug: changed location of storage for repos

--- app_code.py
+++ app_code.py
@@ -14,19 +14,21 @@ from llama_index import (
 )
 from llama_index.llms import OpenAI
 
+current_dir = os.getcwd()
+
 def load_and_unzip_file(uploaded_file):
     with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
-        zip_ref.extractall('./code')
+        zip_ref.extractall(current_dir)
     repo_name = uploaded_file.name.replace(".zip", "")
-    clone_path = f"./code/{repo_name}"
-    GPTRepoReader = download_loader("GPTRepoReader")
+    clone_path = f"{current_dir}/{repo_name}"
+    GPTRepoReader = download_loader("GPTRepoReader", custom_path=current_dir)
     loader = GPTRepoReader()
-    documents = loader.load_data(clone_path)
+    documents = loader.load_data(repo_path=clone_path)
     return documents
 
 def delete_unzipped_folder(uploaded_file):
     repo_name = uploaded_file.name.replace(".zip", "")
-    clone_path = f"./{repo_name}"
+    clone_path = f"{current_dir}/{repo_name}"
     if os.path.exists(clone_path):
         shutil.rmtree(clone_path)
 
