1413ff91073d2f66d9cda639cc68246f3402aac9
paulwababu <paulkiragu621@gmail.com>
2023-11-08 20:58:43 +0300

    bug: fixed issue with clone path

--- app_code.py
+++ app_code.py
@@ -19,7 +19,7 @@ def load_and_unzip_file(uploaded_file):
         zip_ref.extractall('/tmp')
     repo_name = uploaded_file.name.replace(".zip", "")
     clone_path = f"/tmp/{repo_name}"
-    GPTRepoReader = download_loader("GPTRepoReader", custom_path=clone_path)
+    GPTRepoReader = download_loader("GPTRepoReader", custom_path=os.getcwd())
     loader = GPTRepoReader()
     documents = loader.load_data(repo_path=clone_path)
     return documents
