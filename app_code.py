a511886ca0398d3b7ca51c1fcd169ab83af909cb
paulwababu <paulkiragu621@gmail.com>
2023-11-08 18:45:21 +0300

    bug: changed location of storage for repo

--- app_code.py
+++ app_code.py
@@ -19,7 +19,7 @@ def load_and_unzip_file(uploaded_file):
         zip_ref.extractall('./')
     repo_name = uploaded_file.name.replace(".zip", "")
     clone_path = f"./{repo_name}"
-    GPTRepoReader = download_loader("GPTRepoReader", custom_path=clone_path)
+    GPTRepoReader = download_loader("GPTRepoReader")
     loader = GPTRepoReader()
     documents = loader.load_data()
     return documents
