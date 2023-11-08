7cb092c0a315b8920913123a69bbc73f28615e37
paulwababu <paulkiragu621@gmail.com>
2023-11-08 17:50:28 +0300

    bug: changed location of storage for repos

--- app_code.py
+++ app_code.py
@@ -19,7 +19,7 @@ def load_and_unzip_file(uploaded_file):
     with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
         zip_ref.extractall(os.getcwd())
     repo_name = uploaded_file.name.replace(".zip", "")
-    clone_path = os.getcwd() / repo_name
+    clone_path = os.getcwd()
     GPTRepoReader = download_loader("GPTRepoReader")
     loader = GPTRepoReader()
     documents = loader.load_data(repo_path=clone_path)
@@ -27,7 +27,7 @@ def load_and_unzip_file(uploaded_file):
 
 def delete_unzipped_folder(uploaded_file):
     repo_name = uploaded_file.name.replace(".zip", "")
-    clone_path = os.getcwd() / repo_name
+    clone_path = os.getcwd()
     if os.path.exists(clone_path):
         shutil.rmtree(clone_path)
 
